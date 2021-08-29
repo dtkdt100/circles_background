709b2a387f73a65f889fa5d615da53033c3acf46
Dolev Franco <dolevfranco2005@gmail.com>
2021-08-29 19:18:57 +0300

    first commit

---
+++ lib/circles_background.dart
@@ -0,0 +1,4 @@
+library circles_background;
+
+export 'circles_background/circles_builder.dart';
+export 'circles_background/three_circles_builder.dart';
---
+++ lib/circles_background/circles_builder.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CircleInfo {
+  CircleInfo({
+    required this.size,
+    this.turns = 0,
+    this.alignment = Alignment.topCenter,
+    this.borderRadius,
+    this.gradient,
+  });
+
+  final Size size;
+  final double turns;
+  final Alignment alignment;
+  final BorderRadiusGeometry? borderRadius;
+  final Gradient? gradient;
+}
+
+class CirclesBackground extends StatelessWidget {
+  CirclesBackground({required this.circles, this.child});
+
+  final Widget? child;
+  final List<CircleInfo> circles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Stack(
+          children: [
+            Stack(
+              children: List.generate(
+                  circles.length, (index) => buildCircle(circles[index])),
+            ),
+            Theme.of(context).brightness == Brightness.dark ? Container(
+              color: Colors.black.withOpacity(0.6),
+            ) : SizedBox(),
+          ],
+        ),
+        child ?? SizedBox(),
+      ],
+    );
+  }
+
+  Widget buildCircle(CircleInfo circle) => Align(
+        alignment: circle.alignment,
+        child: RotationTransition(
+          turns: AlwaysStoppedAnimation(circle.turns),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: circle.borderRadius,
+              gradient: circle.gradient,
+            ),
+            width: circle.size.width,
+            height: circle.size.height,
+          ),
+        ),
+      );
+}
